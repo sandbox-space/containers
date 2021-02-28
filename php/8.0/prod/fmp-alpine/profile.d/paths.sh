d7e8e14469aaea47136c322df9f600555e6b7538
Tomas Kubat <tomas.kubat@gmail.com>
2021-02-28 21:32:46 +0100

    Initial containers nginx, php, mariadb

---
+++ php/8.0/prod/fmp-alpine/profile.d/paths.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+SRC_PATH="/src"
+export APP_ROOT="$SRC_PATH"
+export APP_LOG="$SRC_PATH/log"
+export APP_TMP="$SRC_PATH/tmp"
---
+++ php/8.0/prod/fmp-alpine/profile.d/prompt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+NORMAL="\[\e[0m\]"
+RED="\[\e[1;31m\]"
+GREEN="\[\e[1;32m\]"
+if [ "$USER" = root ]; then
+        PS1="$RED\u@\h [$NORMAL\w$RED]# $NORMAL"
+else
+        PS1="$GREEN\u@\h [$NORMAL\w$GREEN]\$ $NORMAL"
+fi
