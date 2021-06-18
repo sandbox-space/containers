b8e027f52fb36e235fab7e18a7274698672fc11d
Tomas Kubat <tomas.kubat@gmail.com>
2021-06-18 18:04:53 +0200

    Added aliases project-console and project-console-debug

--- php/8.0/prod/fmp-alpine/profile.d/5prompt.sh
+++ php/8.0/prod/fmp-alpine/profile.d/5prompt.sh
@@ -3,7 +3,7 @@ NORMAL="\[\e[0m\]"
 RED="\[\e[1;31m\]"
 GREEN="\[\e[1;32m\]"
 if [ "$USER" = root ]; then
-        PS1="$RED\u@\h [$NORMAL\w$RED]# $NORMAL"
+    PS1="$RED\u@\h [$NORMAL\w$RED]# $NORMAL"
 else
-        PS1="$GREEN\u@\h [$NORMAL\w$GREEN]\$ $NORMAL"
+    PS1="$GREEN\u@\h [$NORMAL\w$GREEN]\$ $NORMAL"
 fi
