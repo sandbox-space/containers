4dabfd0e65df612e5c45bd5d38e49eaebc7417cc
Tomas Kubat <tomas.kubat@gmail.com>
2021-07-25 22:30:28 +0200

    Changed demver-api url and CI schedule for one time per day

--- php/8.0/prod/fmp-alpine/profile.d/9aliases.sh
+++ php/8.0/prod/fmp-alpine/profile.d/9aliases.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
 alias ll='ls -la'
-
-alias project-root="cd $APP_ROOT"
-alias project-log="cd $APP_LOG"
-alias project-tmp="cd $APP_TMP"
-
-alias project-console="$APP_ROOT/bin/console $@"
-alias project-console-debug="XDEBUG_CONFIG='idekey=PHPSTORM' PHP_IDE_CONFIG='serverName=project-console' project-console $@"
